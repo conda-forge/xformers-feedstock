ee2b6692d788370b14918ac8af23d1ed091cb991
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2024-05-29 19:34:23 -0500

    Update TORCH CUDA ARCH LIST
    https://github.com/conda-forge/pytorch_sparse-feedstock/blob/main/recipe/build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,29 @@
-set -ex
+#!/bin/bash
+
+set -euxo pipefail
 
 # Target the same CUDA archs as conda-forge pytorch package
 # https://github.com/conda-forge/pytorch-cpu-feedstock/blob/main/recipe/build_pytorch.sh
 # Number of CUDA archs reduced to fit CI resources
-if [[ ${cuda_compiler_version} != "None" ]]; then
-    if [[ ${cuda_compiler_version} == 11.0* ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5+PTX;6.0;7.0;8.0+PTX"
+if [[ ${cuda_compiler_version} != "None" && "$target_platform" == linux-64 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0"
+    export FORCE_CUDA="1"
+    if [[ ${cuda_compiler_version} == 9.0* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;7.0+PTX"
+    elif [[ ${cuda_compiler_version} == 9.2* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0+PTX"
+    elif [[ ${cuda_compiler_version} == 10.* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5+PTX"
+    elif [[ ${cuda_compiler_version} == 11.0* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0+PTX"
     elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5+PTX;6.0;7.0;8.0+PTX"
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
     elif [[ ${cuda_compiler_version} == 11.2 ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5+PTX;6.0;7.0;8.0+PTX"
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+    elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+    elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+        export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     else
         echo "Unsupported CUDA compiler version. Edit build.sh to add target CUDA archs."
         exit 1
@@ -17,4 +31,6 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
     export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
 fi
 
+
+echo "Installing"
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
