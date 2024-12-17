f7ebf6509508f8bd4a428495472a17aa1d94bfb0
Hadrien Mary <hadrien.mary@gmail.com>
2024-12-17 11:12:06 -0500

    cuda 12.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ set -ex
 if [[ ${cuda_compiler_version} != "None" ]]; then
     if [[ ${cuda_compiler_version} == 11.8 ]]; then
         export TORCH_CUDA_ARCH_LIST="5.3;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
-    elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+    elif [[ ${cuda_compiler_version} == 12.6 ]]; then
         export TORCH_CUDA_ARCH_LIST="5.3;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     else
         echo "Unsupported CUDA compiler version. Edit build.sh to add target CUDA archs."
